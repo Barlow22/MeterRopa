80b846ff5bf6511237efdce8d2686368e1758d72
Barlow22 <hartman.lucasj@gmail.com>
2020-06-22 17:49:02 -0300

    ultima version

--- meterRopa.py
+++ meterRopa.py
@@ -11,19 +11,18 @@ class SearchCases(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome('chromedriver.exe')
         self.driver.get('http://automationpractice.com/index.php')
+        self.driver.implicitly_wait(5)
         self.index_page = PageIndex(self.driver)
         self.items_page = PageItems(self.driver)
         self.shop_page = PageShop(self.driver)
 
+    # @unittest.skip('not needed now')
     def test_search_tshirts(self):
         
         self.index_page.search_items('t-shirts')
-        time.sleep(2)
         self.items_page.pick_color()
-        time.sleep(2)
         self.shop_page.quantity('25')
         self.shop_page.sum_quantity(3)
-        time.sleep(2)
         self.assertEqual(self.shop_page.return_quantity() , '28')
 
     def tearDown(self):
--- pageIndex.py
+++ pageIndex.py
@@ -1,14 +1,19 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 class PageIndex:
 
 
     def __init__(self, my_driver):
         self.driver = my_driver
-        self.search_query_top  = 'search_query_top'
-        self.submit_search = 'submit_search'
-        self.color = 'color_1'
+        self.query_top = (By.ID, 'search_query_top' )
+        self.submit_search = (By.NAME, 'submit_search')
+        
 
     def search_items(self, item):
-        self.driver.find_element_by_id(self.search_query_top).send_keys(item)
-        self.driver.find_element_by_name(self.submit_search).click()
+        search_box = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.query_top))
+        self.driver.find_element(*self.query_top).send_keys(item)
+        self.driver.find_element(*self.submit_search).click()
     
 
